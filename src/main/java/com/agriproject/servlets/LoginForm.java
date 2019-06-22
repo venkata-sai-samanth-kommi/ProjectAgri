e80c009c3ed4bb1004e6bc033f9f04d46e22a741
Sai samanth <sai12999.k@gmail.com>
2019-06-22 20:15:29 +0530

    servlets added

--- src/main/java/com/agriproject/servlets/LoginForm.java
+++ src/main/java/com/agriproject/servlets/LoginForm.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginForm
  */
-@WebServlet("LoginForm")
+@WebServlet("/LoginForm")
 public class LoginForm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
